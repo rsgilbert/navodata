a5688c43c7d03b8ad6cedaf527c0ee1a226ea75a
SSG <gssenyonjo@gmail.com>
2023-10-13 13:14:36 +0300

    Updated filter query to support any kind of operator. Made sure that for ISO dates we dont surround the date value with single quotes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const prepare = (baseUrl, companyName) => {
     // about missing company information when you make PATCH 
     // requests using the Company('MyNiceCompany') approach
     addParam(companyBaseUrl, 'company', companyName)
-    
+
     return query
 
     // Below function will be hoisted
@@ -14,10 +14,10 @@ const prepare = (baseUrl, companyName) => {
         serviceName,
         id,
         count,
-        top, 
+        top,
         skip,
         // filter is an object / array. If object, it has properties: property, and one of these: endswith, startswith, equals, contains
-        filter, 
+        filter,
         orderby,
         select,
         // For Odata V4 (> MS Dynamics 2018)
@@ -29,14 +29,14 @@ const prepare = (baseUrl, companyName) => {
         testField('serviceName', serviceName)
         addToPath(url, serviceName)
         // id
-        if(id) {
-            if(typeof id == "number") {
+        if (id) {
+            if (typeof id == "number") {
                 url.pathname += `(${id})`
-            } 
-            else if(typeof id == "object") {
+            }
+            else if (typeof id == "object") {
                 // We are specifying multiple keys
-                let idParts = Object.entries(id).map(([k,v]) => {
-                    if(typeof v === 'number') {
+                let idParts = Object.entries(id).map(([k, v]) => {
+                    if (typeof v === 'number') {
                         return `${k}=${v}`
                     } else {
                         // assume v is a string
@@ -52,43 +52,43 @@ const prepare = (baseUrl, companyName) => {
         }
 
         // count
-        if(count) {
+        if (count) {
             testField('id', id, undefined)
             addParam(url, '$count', 'true')
         }
 
         // top
-        if(top) {
+        if (top) {
             testField('id', id, undefined)
             addParam(url, '$top', top)
         }
 
         // skip
-        if(skip) {
+        if (skip) {
             testField('id', id, undefined)
             addParam(url, '$skip', skip)
         }
 
         // orderby
-        if(orderby) {
+        if (orderby) {
             testField('id', id, undefined)
             let orderbyParamValue = orderby.property
-            if(orderby.isDescending) {
+            if (orderby.isDescending) {
                 orderbyParamValue += ' desc'
-            } 
+            }
             addParam(url, '$orderby', orderbyParamValue)
         }
 
         // select
-        if(select) {
+        if (select) {
             testField('id', id, undefined)
             const selectParamValue = select.join(',')
             addParam(url, '$select', selectParamValue)
         }
 
         // filter
-        if(filter) {
-            if(filter instanceof Array) {
+        if (filter) {
+            if (filter instanceof Array) {
                 let filters = filter.map(f => {
                     return getFilterParamValue(f)
                 })
@@ -96,7 +96,7 @@ const prepare = (baseUrl, companyName) => {
                 addParam(url, '$filter', paramValue)
             }
             else {
-                addParam(url, '$filter', getFilterParamValue(filter))            
+                addParam(url, '$filter', getFilterParamValue(filter))
             }
         }
 
@@ -111,14 +111,14 @@ const prepare = (baseUrl, companyName) => {
         // Also note that boundedActions begin with: "NAV." eg NAV.PerformPost
         // Moreover, in the json for the request body, ensure all your keys begin with a small letter, eg
         // use { requestId: 52 } and not { RequestId: 52 } lest you get a BadRequest error.
-        if(boundedAction) {
+        if (boundedAction) {
             addToPath(url, boundedAction)
         }
 
         // json support
         // if you donot want to use json, and the web service eg for old NAV versions like NAV 2015
         // supports or defaults to xml, set property -> disableJson: true
-        if(!disableJson) {
+        if (!disableJson) {
             addParam(url, '$format', 'json')
         }
         return url.href
@@ -126,13 +126,13 @@ const prepare = (baseUrl, companyName) => {
 }
 
 function getFilterParamValue(filter) {
-    if(filter.contains) {
+    if (filter.contains) {
         const containsParamValue = `contains(${filter.property},'${filter.contains}')`
         return containsParamValue
     }
-    if(filter.equals) {
+    if (filter.equals) {
         let equalsParamValue
-        if(typeof filter.equals === 'number' || typeof filter.equals === 'boolean') {
+        if (typeof filter.equals === 'number' || typeof filter.equals === 'boolean') {
             equalsParamValue = `${filter.property} eq ${filter.equals}`
         }
         else {
@@ -140,16 +140,69 @@ function getFilterParamValue(filter) {
         }
         return equalsParamValue
     }
-    if(filter.endswith) {
+    if (filter.endswith) {
         const endswithParamValue = `endswith(${filter.property},'${filter.endswith}')`
         return endswithParamValue
     }
-    if(filter.startswith) {
+    if (filter.startswith) {
         const startswithParamValue = `startswith(${filter.property},'${filter.startswith}')`
         return startswithParamValue
     }
-    throw Error('Unknown filter: ' + JSON.stringify(filter))
+    else {
+        // we assume its an operator
+        // visit this link for info about operators: https://docs.oasis-open.org/odata/odata/v4.01/odata-v4.01-part1-protocol.html#_Toc31358948
+        let keys = Object.keys(filter)
+        let operator = keys.find(k => k !== 'property')
+        let value = filter[operator]
+
+        let operatorParamValue
+
+        if (typeof value === 'number' || typeof value === 'boolean') {
+            operatorParamValue = `${filter.property} ${operator} ${value}`
+        }
+        if (value instanceof Date || isIsoDate(value)) {
+            // we are dealing with a date 
+            let isoDate = value
+            if (value instanceof Date) {
+                isoDate = value.toISOString()
+            }
+            operatorParamValue = `${filter.property} ${operator} ${isoDate}`
+        }
+        else {
+            operatorParamValue = `${filter.property} ${operator} '${value}'`
+        }
+        return operatorParamValue
+    }
+    // throw Error('Unknown filter: ' + JSON.stringify(filter))
 }
+
+
+/**
+ * 
+ * @param {string} str 
+ * @returns {boolean}
+ */
+function isIsoDate(str) {
+    if (str.includes('T')) {
+        // this is datetime
+        if (!/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/.test(str)) return false;
+    }
+    else {
+        // this is date only
+        if (!/\d{4}-\d{2}-\d{2}/.test(str)) return false;
+    }
+    const d = new Date(str);
+    return d instanceof Date && !isNaN(d.getTime()) && d.toISOString().startsWith(str); // valid date 
+}
+
+// console.log(isIsoDate('ttd'))
+// console.log(isIsoDate('2022-01-01'))
+// console.log(isIsoDate('2011-10-05T14:48:00.000Z'))
+
+// console.log(isIsoDate('2018-11-10T11:22:33+00:00'));
+
+// console.log(isIsoDate('2011-10-05T14:99:00.000Z')); // invalid time part 
+
 
 function addToPath(url, value) {
     url.pathname += '/' + value
@@ -162,21 +215,21 @@ function addParam(url, paramKey, paramValue) {
 
 // Throw error if the field is not matching our expectations
 function testField(label, fieldValue, expectedValue) {
-    if(arguments.length === 2) {
-        if(!fieldValue) {
+    if (arguments.length === 2) {
+        if (!fieldValue) {
             throw new Error(`Field "${label}" must have a value`)
         }
     }
 
-    else if(expectedValue === undefined) {
-        if(fieldValue !== undefined) {
+    else if (expectedValue === undefined) {
+        if (fieldValue !== undefined) {
             throw Error(`Field "${label}" must NOT have a value`)
         }
     }
 
     // We stringify so our check works for objects as well
     else {
-        if(JSON.stringify(fieldValue) !== JSON.stringify(expectedValue)) {
+        if (JSON.stringify(fieldValue) !== JSON.stringify(expectedValue)) {
             throw Error(`Field "${label}" must equal ${expectedValue} but was ${fieldValue}`)
         }
     }
